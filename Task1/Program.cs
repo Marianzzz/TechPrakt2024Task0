aaf1605689ee37b91f0b8845e2a6d1b7962dcb3c
Marian Tsyvinskyi <mariantop228@gmail.com>
2024-01-27 20:01:08 +0200

    Виконаний проект

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.Write("Введіть перше число:");
+double number1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введіть друге число:");
+double number2 = Convert.ToDouble(Console.ReadLine());
+
+double sum = number1+ number2;
+Console.WriteLine($"Сума чисел:{sum}");
